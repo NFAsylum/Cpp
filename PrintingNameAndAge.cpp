a7182133dcd6eda05eaed61daa2d64f0fea4f716
NFAsylum <119449613+NFAsylum@users.noreply.github.com>
2024-04-11 15:47:35 -0300

    Create PrintingNameAndAge.cpp

---
+++ PrintingNameAndAge.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string characterName = "Mark";
+    int characterAge = 20;
+
+    cout << characterName << " is " << characterAge << " years old" << endl;
+    return 0;
+}
