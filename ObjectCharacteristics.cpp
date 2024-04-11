3764c94878d5f3b37bb0786adec8da7c38a3de21
NFAsylum <119449613+NFAsylum@users.noreply.github.com>
2024-04-11 16:00:15 -0300

    Create ObjectCharacteristics.cpp

---
+++ ObjectCharacteristics.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string objectName = "table";
+    char objectModel = 'B';
+    int objectQuantity = 20;
+    double objectSize = 15.2;
+    bool objectIsAssembled = true;
+
+    cout << "Object information: \n";
+    cout << "Name: " << objectName << "    ";
+    cout << "Model: " << objectModel << "    ";
+    cout << "Quantity: " << objectQuantity << endl;
+    cout << "Size: " << objectSize << "    ";
+    cout << "Is Assembled: " << objectIsAssembled;
+
+
+
+    return 0;
+}
