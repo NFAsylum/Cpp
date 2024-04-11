aa278b9adaa626739b73e24ee97ab138fd7f1d22
NFAsylum <119449613+NFAsylum@users.noreply.github.com>
2024-04-11 16:19:30 -0300

    Create MathFunctions.cpp

---
+++ MathFunctions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    cout << pow(3, 3) << endl;
+    cout << sqrt(9) << endl;
+    cout << round(12.0234) << endl;
+    cout << ceil(6.1) << endl;
+    cout << floor(6.9) << endl;
+    cout << fmax(10, 100) << endl;
+    cout << fmin(10, 100) << endl;
+
+    return 0;
+}
