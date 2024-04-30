1c926df33d039b24ed61abb1349c4a127a63e7cf
NFAsylum <119449613+NFAsylum@users.noreply.github.com>
2024-04-29 23:22:44 -0300

    Create TernaryOperator.cpp

---
+++ Basics/TernaryOperator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i = 0;
+
+    i = i > 0 ? 1 : 0;
+    cout << i << endl;
+
+    i = 1;
+    i = i > 0 ? 1 : 0;
+    cout << i << endl;
+
+    return 0;
+}
