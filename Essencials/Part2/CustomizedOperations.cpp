596f8899241d13c700f814e41a1bfe0b803c548e
NFAsylum <119449613+NFAsylum@users.noreply.github.com>
2024-08-09 16:37:14 -0300

    Update CustomizedOperations.cpp
    Added new test operations showing the use of the Fraction Class

--- Essencials/Part2/CustomizedOperations.cpp
+++ Essencials/Part2/CustomizedOperations.cpp
@@ -117,4 +117,22 @@ int main(){
     Fraction n1(15), n2(3);
     n1 = n1 * n2;
     std::cout << n1;
+
+    Fraction f1(1,2), f2(2,3), f;
+
+    cout << f1 << "->" << f1.get_double() << endl;
+    cout << f2 << "->" << f2.get_double() << endl;
+    f = f1 + f2;
+    cout << f1 << "+" << f2 << "=" << f << endl;
+    f = f2 + f2 + f2;
+    cout << f2 << "+" << f2 << "+" << f2 << "=" << f << endl;   
+    f = !f;
+    cout << f2 << "+" << f2 << "+" << f2 << "=" << f << endl;
+    f = f1 * f2;
+    cout << f1 << "*" << f2 << "=" << f << endl;
+    f = f1 / f2;
+    cout << f1 << ":" << f2 << "=" << f << endl;
+    Fraction f3(7,8);
+    f3 += f1;
+    cout << f3 << endl;
 }
